efe0386fecbc1c2865f3181ba34fa20a45bc5e26
Pablohsk <pablofidelisdias@gmail.com>
2024-05-19 01:49:18 -0300

    Criado código para ClienteService em service

--- src/main/java/com/microsoft/gestao_projetos/service/ClienteService.java
+++ src/main/java/com/microsoft/gestao_projetos/service/ClienteService.java
@@ -1,4 +1,24 @@
 package com.microsoft.gestao_projetos.service;
 
+import com.microsoft.gestao_projetos.models.Cliente;
+import com.microsoft.gestao_projetos.repositories.ClienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class ClienteService {
+
+    @Autowired
+    private ClienteRepository clienteRepository;
+
+    public List<Cliente> findAll() {
+        return clienteRepository.findAll();
+    }
+
+    public Cliente save(Cliente cliente) {
+        return clienteRepository.save(cliente);
+    }
+
 }
