a6fb09d7745b7587ff52a91524675ce9672a67ee
Pablohsk <pablofidelisdias@gmail.com>
2024-05-19 01:34:47 -0300

    Criado código para Atividade em models

---
+++ src/main/java/com/microsoft/gestao_projetos/models/Atividade.java
@@ -0,0 +1,48 @@
+package com.microsoft.gestao_projetos.models;
+import jakarta.persistence.*;
+
+@Entity
+public class Atividade {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String descricao;
+    private String status;
+
+    @ManyToOne
+    @JoinColumn(name = "projeto_id")
+    private Projeto projeto;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Projeto getProjeto() {
+        return projeto;
+    }
+
+    public void setProjeto(Projeto projeto) {
+        this.projeto = projeto;
+    }
+}
