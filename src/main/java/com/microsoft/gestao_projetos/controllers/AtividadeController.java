e87aabaa9d1eb27ec9479d58f0b69c4ea42de93d
Pablohsk <pablofidelisdias@gmail.com>
2024-05-19 01:58:12 -0300

    Criado código para AtividadeController em controllers

--- src/main/java/com/microsoft/gestao_projetos/controllers/AtividadeController.java
+++ src/main/java/com/microsoft/gestao_projetos/controllers/AtividadeController.java
@@ -1,4 +1,32 @@
 package com.microsoft.gestao_projetos.controllers;
 
+import com.microsoft.gestao_projetos.models.Atividade;
+import com.microsoft.gestao_projetos.service.AtividadeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/atividades")
 public class AtividadeController {
+
+    @Autowired
+    private AtividadeService atividadeService;
+
+    @GetMapping
+    public List<Atividade> getAllAtividades() {
+        return atividadeService.findAll();
+    }
+
+    @GetMapping("/projeto/{projetoId}")
+    public List<Atividade> getAtividadesByProjeto(@PathVariable Long projetoId) {
+        return atividadeService.findByProjetoId(projetoId);
+    }
+
+    @PostMapping
+    public Atividade createAtividade(@RequestBody Atividade atividade) {
+        return atividadeService.save(atividade);
+    }
+
 }
