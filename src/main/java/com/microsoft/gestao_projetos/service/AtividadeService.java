5becb1f90f151322e03a3872cccf2caf2e93ba0a
Pablohsk <pablofidelisdias@gmail.com>
2024-05-19 01:52:17 -0300

    Criado código para AtividadeService em service

--- src/main/java/com/microsoft/gestao_projetos/service/AtividadeService.java
+++ src/main/java/com/microsoft/gestao_projetos/service/AtividadeService.java
@@ -1,4 +1,27 @@
 package com.microsoft.gestao_projetos.service;
 
+import com.microsoft.gestao_projetos.models.Atividade;
+import com.microsoft.gestao_projetos.repositories.AtividadeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class AtividadeService {
+
+    @Autowired
+    private AtividadeRepository atividadeRepository;
+
+    public List<Atividade> findAll() {
+        return atividadeRepository.findAll();
+    }
+
+    public List<Atividade> findByProjetoId(Long projetoId) {
+        return atividadeRepository.findByProjetoId(projetoId);
+    }
+
+    public Atividade save(Atividade atividade) {
+        return atividadeRepository.save(atividade);
+    }
 }
